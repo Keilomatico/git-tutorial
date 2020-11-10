b5a812a399c4262ea3b0abaa227a4d7900c62213
Adrian Keil <adrian.keil@tum.de>
2020-11-10 11:37:19 +0100

    Implemented first part of the coding challenge: User can input a number which gets converted to text and is outputted (but in reversed order)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include "espl_lib.h"
 
 int main() {
-	printf("Hello ESPL\n");
+	unsigned int num;
+	char *output;
+
+	printf("Please input a number: ");
+	scanf("%d", &num);
+	
+	output = num_to_words(num);
+
+	unsigned int i = 0;
+	while (output[i] != '\0') 
+	{
+		printf("%c", output[i]);
+		i++;
+   	}
+
+	printf("\n");
+
 	return 0;
 }
