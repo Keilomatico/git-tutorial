0bbff01274f95d3363aaf0c70dfb0e199d90f01b
Adrian Keil <adrian.keil@tum.de>
2020-11-10 12:09:40 +0100

    User is now promted if he wants to exit the program or run another number

--- src/main.c
+++ src/main.c
@@ -1,23 +1,42 @@
 #include<stdio.h>
 #include "espl_lib.h"
 
-int main() {
+int main() 
+{
 	unsigned int num;
 	char *output;
+	char cont, temp;
 
-	printf("Please input a number: ");
-	scanf("%d", &num);
+	while(1)
+	 {
+
+		printf("Please input a number: ");
+		scanf("%d%c", &num, &temp);
 	
-	output = num_to_words(num);
+		output = num_to_words(num);
 
-	unsigned int i = 0;
-	while (output[i] != '\0') 
-	{
-		printf("%c", output[i]);
-		i++;
-   	}
+		unsigned int i = 0;
+		while (output[i] != '\0') 
+		{
+			printf("%c", output[i]);
+			i++;
+   		}
 
-	printf("\n");
+		printf("\n");	
+
+		cont = ' ';
+		while(cont != 'Y' && cont != 'y' && cont != 'N' && cont != 'n')
+		{
+			printf("\nDo you want to run another number [Y/N]? ");
+			scanf("%c%c", &cont, &temp);
+		}
+
+		if(cont != 'Y' && cont != 'y')
+		{
+			break;
+		}
+
+	}
 
 	return 0;
 }
